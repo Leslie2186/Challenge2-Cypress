af17e4d603c9a287c466ad3b8ec2388ac3aa7033
Leslie2186 <leslie.moraud@gmail.com>
2024-05-22 09:26:42 +0200

    Challenge 2 terminé

--- cypress/e2e/test-auth-reussi.cy.js
+++ cypress/e2e/test-auth-reussi.cy.js
@@ -1,25 +1,28 @@
-describe("Test Auth pass", () => {
-    it("The url is ok", () => {
-        cy.visit("https://preprod.backmarket.fr/fr-fr/register")
+describe("Test Inscription", () => {
+    it("Inscription is ok", () => {
+        cy.visit("https://preprod.backmarket.fr/fr-fr/register");
+        cy.get("#firstName").click({force: true}).type("Toto");
+        cy.get("#lastName").click({force: true}).type("Dupond");
+        cy.get("#signup-email").click({force: true}).type("toto@dupond.com");
+        cy.get("#signup-password").click({force: true}).type("1234Toto");
+        cy.get('[data-qa="signup-submit-button"]').click({force: true});
     });
 
-    it("The type of the firstname is text", () =>{
-        expect(cy.get("#firstname")).to.be.a('string')
+    it("Inscription is not ok", () => {
+        cy.visit("https://preprod.backmarket.fr/fr-fr/register");
+        cy.get("#firstName").click({force: true}).type("Roger");
+        cy.get("#lastName").click({force: true}).type("");
+        cy.get("#signup-email").click({force: true}).type("roger@Dupond.com");
+        cy.get("#signup-password").click({force: true}).type("1234Roger");
+        cy.get('[data-qa="signup-submit-button"]').click({force: true});
+    });
+});
+
+describe("Test Authentification", () => {
+    it("Auth is ok", () => {
+        cy.visit("https://preprod.backmarket.fr/fr-fr/register");
+        cy.get("#signup-email").click({force: true}).type("lilie@gmail.com");
+        cy.get("#signup-password").click({force: true}).type("Lilie33!");
     });
 
-    it("The type of the lastname is text", () =>{
-        expect(cy.get("#lastname")).to.be.a('string')
-    });
-
-    it("The type of the email is email", () =>{
-        expect(cy.get("#signup-email")).to.be.an('email')
-    });
-
-    it("The type of the password is text", () =>{
-        expect(cy.get("#signup-password")).to.be.a('string')
-    });
-
-    it("Clic on the button", () =>{
-        cy.get("button").click()
-    });
 });
