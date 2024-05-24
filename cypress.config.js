acc9576d9535f067b0f61ae6d681edae8ca95228
Leslie2186 <leslie.moraud@gmail.com>
2024-05-24 11:14:10 +0200

    Modification test-auth-reussi et test avec mailslurp

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'fx3ypu',
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+  env: {
+      MAILSLURP_API_KEY:
+      "09b4d1795367d63c6e249085a55da3e2db56dcbba10a3fd882527ae8c347d458"
     },
-  },
+    e2e: {
+      defaultCommandTimeout: 40000,
+      responseTimeout: 40000,
+      requestTimeout: 40000
+    },
 });
--- cypress/e2e/test-auth-reussi.cy.js
+++ cypress/e2e/test-auth-reussi.cy.js
@@ -1,29 +1,30 @@
 describe("Test Inscription", () => {
     it("Inscription is ok", () => {
         cy.visit("https://preprod.backmarket.fr/fr-fr/register");
-        cy.get("#firstName").click({force: true}).type("Toto");
-        cy.get("#lastName").click({force: true}).type("Dupond");
-        cy.get("#signup-email").click({force: true}).type("toto@dupond.com");
-        cy.get("#signup-password").click({force: true}).type("1234Toto");
-        cy.get('[data-qa="signup-submit-button"]').click({force: true});
+        cy.get("#firstName").click().type("Lilie33");
+        cy.get("#lastName").click().type("Dupond");
+        cy.get("#signup-email").click().type("lilie33@dupond.com");
+        cy.get("#signup-password").click().type("1234Lilie33");
+        cy.get("#newsletter").check({force:true});
+        cy.get('[data-qa="signup-submit-button"]').click();
     });
 
     it("Inscription is not ok", () => {
         cy.visit("https://preprod.backmarket.fr/fr-fr/register");
-        cy.get("#firstName").click({force: true}).type("Roger");
-        cy.get("#lastName").click({force: true}).type("");
-        cy.get("#signup-email").click({force: true}).type("roger@Dupond.com");
-        cy.get("#signup-password").click({force: true}).type("1234Roger");
-        cy.get('[data-qa="signup-submit-button"]').click({force: true});
+        cy.get('[data-qa="signup-submit-button"]').click();
+        cy.get("p.body-2-light").should('be.visible').and("contain.text", 'Le champ "Prénom" est obligatoire');
+        cy.get("p.body-2-light").should('be.visible').and("contain.text", 'Le champ "Nom" est obligatoire');
+        cy.get("p.body-2-light").should('be.visible').and("contain.text", 'Le champ "Email" est obligatoire');
+        cy.get("p.body-2-light").should('be.visible').and("contain.text", 'Le champ mot de passe est obligatoire');
     });
 });
 
 describe("Test Authentification", () => {
     it("Auth is ok", () => {
         cy.visit("https://preprod.backmarket.fr/fr-fr/register");
-        cy.get("#signup-email").click({force: true}).type("lilie@gmail.com");
-        cy.get("#signup-password").click({force: true}).type("Lilie33!");
-        cy.contains("Welcome Back!").click({force: true});
+        cy.get("#signup-email").click().type("lilie33@dupond.com");
+        cy.get("#signup-password").click().type("1234Lilie33");
+        cy.contains("Welcome Back!").click();
     });
 
 });
